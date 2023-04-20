746c98aa0a1ea2ee16f11f2efdf46dbfda797237
omid <omd.reza.masoumi@gmail.com>
2023-04-21 02:16:51 +0330

    Signup start base

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,13 @@
 package com.example;
 
+import com.example.view.SignupMenu;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        SignupMenu signupMenu = SignupMenu.getInstance();
+        signupMenu.run(scanner);
     }
 }
--- src/main/java/com/example/view/SignupMenu.java
+++ src/main/java/com/example/view/SignupMenu.java
@@ -1,5 +1,30 @@
 package com.example.view;
 
+import com.example.controller.Commands.SignupMenuCommands;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+
 public class SignupMenu {
-    
+    private static SignupMenu instance;
+    private SignupMenu() {
+    }
+    public static SignupMenu getInstance() {
+        if (instance == null) {
+            instance = new SignupMenu();
+        }
+        return instance;
+    }
+    public void run(Scanner scanner) {
+        String input;
+        Matcher matcher;
+        while (true) {
+            input = scanner.nextLine();
+            if ((matcher = SignupMenuCommands.getMatcher(input, SignupMenuCommands.CREATE_USER)).find())
+                register(matcher);
+        }
+    }
+
+    private void register(Matcher matcher) {
+    }
 }
