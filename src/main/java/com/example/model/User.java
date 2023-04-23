24a3a4d1acbdf0fc999fe921edf6cb2fb1b28ae0
fraxea <moh788672@gmail.com>
2023-04-23 21:48:14 +0330

    UsersData.java created

--- src/main/java/com/example/model/User.java
+++ src/main/java/com/example/model/User.java
@@ -1,36 +1,16 @@
 package com.example.model;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
-import com.google.gson.Gson;
-
-public class User {
-    private static final ArrayList<User> USERS;
-    private static final ArrayList<String> PASSWORD_RECOVERY_QUESTIONS;
-    private final int recoveryQuestionNumber;
-    private final String recoveryAnswer;
+public class User implements PasswordRecoveryQuestions {
     private String username;
     private String password;
     private String nickname;
     private String email;
     private String slogan;
     private int highscore;
+    private final int recoveryQuestionNumber;
+    private final String recoveryAnswer;
 
-    static {
-        USERS = new ArrayList<>(initializeUsers());
-        PASSWORD_RECOVERY_QUESTIONS = new ArrayList<>();
-        PASSWORD_RECOVERY_QUESTIONS.add("What is my father's name?");
-        PASSWORD_RECOVERY_QUESTIONS.add("What was my first pet's name?");
-        PASSWORD_RECOVERY_QUESTIONS.add("What is my mother's last name?");
-    }
-
-    public User(String username, String password, String nickname, String email, String slogan, int recoveryQuestionNumber, String recoveryAnswer) {
+    protected User(String username, String password, String nickname, String email, String slogan, int recoveryQuestionNumber, String recoveryAnswer) {
         this.username = username;
         this.password = password;
         this.nickname = nickname;
@@ -39,47 +19,6 @@ public class User {
         this.recoveryQuestionNumber = recoveryQuestionNumber;
         this.recoveryAnswer = recoveryAnswer;
         this.highscore = 0;
-    }
-
-    private static List<User> initializeUsers() {
-        Gson gson = new Gson();
-        File main = new File("src", "main");
-        File resources = new File(main, "resources");
-        File json = new File(resources, "json");
-        File users = new File(json, "Users.json");
-        try {
-            users.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            Scanner scanner = new Scanner(users);
-            String information = scanner.nextLine();
-            scanner.close();
-            return List.of(gson.fromJson(information, User[].class));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static void writeUsersInFile() {
-        Gson gson = new Gson();
-        File main = new File("src", "main");
-        File resources = new File(main, "resources");
-        File json = new File(resources, "json");
-        File users = new File(json, "Users.json");
-        try {
-            FileWriter fileWriter = new FileWriter(users);
-            fileWriter.write(gson.toJson(USERS));
-            fileWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static ArrayList<User> getUsers() {
-        return USERS;
     }
 
     public String getUsername() {
@@ -102,8 +41,8 @@ public class User {
         return highscore;
     }
 
-    public int getRecoveryQuestionNumber() {
-        return recoveryQuestionNumber;
+    public String getRecoveryQuestion() {
+        return PASSWORD_RECOVERY_QUESTIONS[recoveryQuestionNumber];
     }
 
     public void setUsername(String username) {
@@ -111,6 +50,7 @@ public class User {
     }
 
     public void setPassword(String password) {
+        // TODO: sha-256
         this.password = password;
     }
 
@@ -132,12 +72,19 @@ public class User {
 
 
     public boolean isPasswordCorrect(String password) {
+        // TODO: sha-256
         return this.password.equals(password);
     }
 
     public boolean isRecoveryAnswerCorrect(String recoveryAnswer) {
+        // TODO: sha-256
         return this.recoveryAnswer.equals(recoveryAnswer);
     }
 
+
+    private String SHA256Cryptographic(String input) {
+        // TODO: sha-256
+        return null;
+    }
 
 }
