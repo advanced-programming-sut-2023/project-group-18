f6646fea6adf5554d2fbd104ca42d93d0fee4352
omid <omd.reza.masoumi@gmail.com>
2023-04-25 23:16:32 +0330

    added profile menu regexes

--- src/main/java/com/example/controller/Commands/ProfileMenuCommands.java
+++ src/main/java/com/example/controller/Commands/ProfileMenuCommands.java
@@ -1,4 +1,26 @@
 package com.example.controller.Commands;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public enum ProfileMenuCommands {
+    CHANGE_PROFILE("^profile change -u (?<username>\\S+)$"),
+    CHANGE_NICKNAME("^profile change -n (?<nickname>\\S+)$"),
+    CHANGE_PASSWORD("^profile change password (?<fields>(-[a-zA-Z] .+){2})$"),
+    CHANGE_EMAIL("^profile change -e (?<email>\\S+)$"),
+    CHANGE_SLOGAN("^profile change slogan -s (?<slogan>\\S+)$"),
+    REMOVE_SLOGAN("^profile remove slogan$"),
+    DISPLAY_HIGHSCORE("^profile display highscore$"),
+    DISPLAY_RANK("^profile display rank$"),
+    DISPLAY_SLOGAN("^profile display slogan$"),
+    DISPLAY_PROFILE("^profile display$");
+    private final String regex;
+    ProfileMenuCommands(String regex) {
+        this.regex = regex;
+    }
+
+    public static Matcher getMatcher(String input, ProfileMenuCommands command) {
+        Pattern pattern = Pattern.compile(command.regex);
+        return pattern.matcher(input);
+    }
 }
