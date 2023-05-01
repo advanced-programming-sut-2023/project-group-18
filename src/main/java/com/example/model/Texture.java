5d21f9bbc8bc2ddfa837e37b164a8b4b0e9b05fb
fraxea <moh788672@gmail.com>
2023-05-01 17:11:50 +0330

    add texture enum

--- src/main/java/com/example/model/Texture.java
+++ src/main/java/com/example/model/Texture.java
@@ -2,6 +2,21 @@ package com.example.model;
 
 public enum Texture {
     DEFAULT ("default"),
+    GROUND ("ground"),
+    GRAVEL ("gravel"),
+    STONE ("stone"),
+    IRON ("iron"),
+    LAWN ("lawn"),
+    RARE_GRASSLAND ("rare grassland"),
+    OVERGROWN_GRASSLAND ("overgrown grassland"),
+    PETROLEUM ("petroleum"),
+    PLAIN ("plain"),
+    SHALLOW_WATER ("shallow water"),
+    RIVER ("river"),
+    POOL ("pool"),
+    BEACH ("beach"),
+    SEA ("sea")
+
     ;
 
     private final String name;
