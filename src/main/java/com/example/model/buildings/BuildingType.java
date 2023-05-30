d3448f17c7cae2542fdbf62ea6797b3eabda617a
fraxea <moh788672@gmail.com>
2023-05-31 00:47:56 +0330

    remove errors

--- src/main/java/com/example/model/buildings/BuildingType.java
+++ src/main/java/com/example/model/buildings/BuildingType.java
@@ -7,178 +7,178 @@ public enum BuildingType {
     SMALL_STONE_GATEHOUSE("small stone gatehouse",0,Asset.STONE,0, 0,
             Texture.GROUND,100,0,0,0, 8,0, 0,
             20,10,35,null,null,2, Category.GATE,
-            GateMethods.getGateMethods(), 500),
+            500),
     BIG_STONE_GATEHOUSE("big stone gatehouse",0,Asset.STONE,20, 0,Texture.GROUND,
             1,1,1,1, 1,1, 1,1,
             1,1,null,null,3, Category.GATE,
-            GateMethods.getGateMethods(), 1000),
+            1000),
     DRAWBRIDGE("Drawbridge",0,Asset.WOOD,10,0, Texture.POOL,0,0,
             0,0,0, 0,0,0,0,0,
-            null,null,1,Category.BUILDING, null,1),
+            null,null,1,Category.BUILDING, 1),
     LOOKOUT_TOWER("lookout tower",0,Asset.STONE,10,0,null,300,
             0,0,0,0,0,0,
             40,10,30,null,null,1,Category.TOWER,
-            TowerMethods.getTowerMethods(),500),
+            500),
     PERIMETER_TOWER("perimeter tower",0,Asset.STONE,10, 0,Texture.GROUND,
             250,0,0,0, 0,0,0,40,
             10,40,null,null,1, Category.TOWER,
-            TowerMethods.getTowerMethods(),600),
+            600),
     TURRET("turret",0,Asset.STONE,15,0, Texture.GROUND,200,0,
             0,0,0, 0,0, 50,20,
-            40,null,null,2, Category.TOWER, TowerMethods.getTowerMethods(),
+            40,null,null,2, Category.TOWER,
             700),
     SQUARE_TOWER("square tower",0,Asset.STONE,35,0, Texture.GROUND,200,
             0,0,0,0, 0,0,60,20,
-            50,null,null,2, Category.TOWER, TowerMethods.getTowerMethods(),
+            50,null,null,2, Category.TOWER,
             900),
     CIRCLE_TOWER("circle tower",0,Asset.STONE,40,0, Texture.GROUND,200,
             0,0,0,0, 0,0,60,20,
-            100,null,null,2, Category.TOWER, TowerMethods.getTowerMethods(),
+            100,null,null,2, Category.TOWER,
             1000),
     ARMOURY("armoury",0,Asset.WOOD,5,0,
             Texture.GROUND,50,50,0,0,0, 0,0,
             0,0,0,null,null,2, Category.STORAGE,
-            StorageMethods.getStorageMethods(),200),
+            200),
     BARRACKS("barracks",0,Asset.STONE,15,0, Texture.GROUND,50,0,
             0,0,0, 0,0,0,0,
             0,null,null,3, Category.BARRACKS
-            ,BarracksMethods.getBarracksMethods(),200),
+            ,200),
     MERCENARY_POST("mercenary post",0,Asset.WOOD,10,0,
             Texture.GROUND,50,0,0,0,0, 0,0,
             0,0,0,null,null,2, Category.BARRACKS,
-            BarracksMethods.getBarracksMethods(),200),
+            200),
     ENGINEER_GUILD("engineer guild",100,Asset.WOOD,10,0,
             Texture.GROUND,50,0,0,0,0, 0,0,
             0,0,0,null,null,2, Category.BARRACKS,
-            BarracksMethods.getBarracksMethods(),150),
+            150),
     KILLING_PIT("killing pit",0,Asset.WOOD,6,0,
             Texture.GROUND,0,0,0,0,0, 0,0,
             0,0,0,null,null,1, Category.BUILDING,
-            null,1),
+            1),
     INN("inn",100,Asset.WOOD,20,1, Texture.GROUND,60,0,0,
             4,0,5,2,0,0,0, Asset.ALE,
-            null,2, Category.PROCESSING,null,200),
+            null,2, Category.PROCESSING,200),
     MILL("mill",0,Asset.WOOD,20,3,
             Texture.GROUND,200,0,0,0,0, 5,5,
             0,0,0,Asset.WHEAT,Asset.FLOUR,1, Category.PROCESSING
-            ,null,300),
+            ,300),
     IRON_MINE("iron mine",0,Asset.WOOD,20,2, Texture.IRON,10,0,
             0,0,0, 5,0,0,0,0,
-            null,Asset.IRON,1,Category.INDUSTRIAL_BUILDING,null,200),
+            null,Asset.IRON,1,Category.INDUSTRIAL_BUILDING,200),
     MARKET("market",0,Asset.WOOD,5,1, Texture.GROUND,30,0,
             0,0,0, 0,0,0,0,0,
-            null,null,2, Category.BUILDING, MarketMethods.getMarketMethods(),200),
+            null,null,2, Category.BUILDING,200),
     OX_TETHER("ox tether",0,Asset.WOOD,5,1, Texture.GROUND,5,0,
             0,0,0, 0,0, 0,0,
             0,null,null,1, Category.INDUSTRIAL_BUILDING
-            , null,200),
+            , 200),
     PITCH_RIG("pitch rig",0,Asset.WOOD,20,1, Texture.PLAIN,20,0,
             0,0,0, 5,0,0,0,0,
-            null,null,1, Category.BUILDING, null,200),
+            null,null,1, Category.BUILDING, 200),
     QUARRY("quarry",0,Asset.WOOD,20,3, Texture.STONE, 1,30,
             0,0,0, 10,0,0,0,
             0,null,Asset.STONE,2, Category.INDUSTRIAL_BUILDING
-            , null,400),
+            , 400),
     STOCKPILE("stockpile",0,null,0,0, Texture.GROUND,50,
             100,0,0,0, 0,0,0,0,
             0,null,null,2,Category.STORAGE
-            , StorageMethods.getStorageMethods(),50),
+            , 50),
     WOOD_CUTTER("wood cutter",0,Asset.WOOD,3,1, Texture.GROUND,20,0,
             0,0,0, 10,0,0,0,
             0,null,Asset.WOOD,1, Category.INDUSTRIAL_BUILDING
-            , null,50),
+            , 50),
     HOVEL("hovel",0,Asset.WOOD,6,0, Texture.GROUND,50,0,0,
             0,6, 0,0,0,0,0,
-            null,null,2,Category.BUILDING, null,100),
+            null,null,2,Category.BUILDING, 100),
     CHURCH("church",250,null,0,0, Texture.GROUND,100,0,
             0,2,0, 0,0,0,0,
-            0,null,null,3,Category.BUILDING, null
+            0,null,null,3,Category.BUILDING
             ,200),
     CATHEDRAL("cathedral",1000,null,0,0, Texture.GROUND,120,
             0,0,2,0, 0,0,0,0,
             0,null,null,4,Category.BARRACKS
-            , BarracksMethods.getBarracksMethods(),400),
+            , 400),
     ARMOURER("armourer",100,Asset.WOOD,20,1, Texture.GROUND,50,0,
             0,0,0, 2,2,0,0,0,
             Asset.IRON, Asset.METAL_ARMOR,2, Category.GUNSMITH,
-            null,200),
+            200),
     BLACKSMITH_MACE("blacksmith",100,Asset.WOOD,20,1,
             Texture.GROUND,50,0,0,0,0, 2,2,
             0,0,0,Asset.IRON,Asset.MACE,2, Category.GUNSMITH,
-            GunsmithMethods.getGunsmithMethods(),200),
+            200),
     BLACKSMITH_SWORD("blacksmith",100,Asset.WOOD,20,1,
             Texture.GROUND,50,0,0,0,0, 2,2,
             0,0,0,Asset.IRON,Asset.SWORDS,2, Category.GUNSMITH,
-            GunsmithMethods.getGunsmithMethods(),200),
+            200),
     FLETCHER("fletcher",100,Asset.WOOD,20,1, Texture.GROUND,50,0,
             0,0,0, 2,2,0,0,
-            0,Asset.IRON,Asset.MACE,2, Category.GUNSMITH, GunsmithMethods.getGunsmithMethods()
+            0,Asset.IRON,Asset.MACE,2, Category.GUNSMITH
             ,200),
     POLETURNER("poleturner",100,Asset.WOOD,20,1,
             Texture.GROUND,50,0,0,0,0, 2,2,
             0,0,0,Asset.IRON,Asset.MACE,2, Category.GUNSMITH,
-            GunsmithMethods.getGunsmithMethods(),200),
+            200),
     OIL_SMELTER("oil smelter",100,Asset.IRON,10,1, Texture.PETROLEUM,20,
             0,0,0,0, 5,0,0,0,
             0,null,Asset.PITCH,1, Category.INDUSTRIAL_BUILDING
-            , null,200),
+            ,200),
     PITCH_DITCH("pitch ditch",0,Asset.PITCH,2,0, Texture.GROUND,0,0,
             0,0,0, 0,0,0,0,0,
-            null,null,1, Category.BUILDING, null,200),
+            null,null,1, Category.BUILDING, 200),
     CAGED_WAR_DOGS("caged war dogs",100,Asset.WOOD,10,0, Texture.GROUND,50,
             0,0,0,0, 0,0,0,0,
-            0,null,null,2, Category.BUILDING,null
+            0,null,null,2, Category.BUILDING
             ,200),
     SIEGE_TENT("siege tent",0,null,0, 1,Texture.GROUND,50,
             0,0,0, 0,0, 0,0,0,
-            0,null,null,1,Category.BUILDING, null,
+            0,null,null,1,Category.BUILDING,
             200),
     STABLE("stable",400,Asset.WOOD,20,0, Texture.GROUND,50,0,
             0,0,0, 0,0,0,0,
-            0,null,null,3,Category.STABLE, null,
+            0,null,null,3,Category.STABLE,
             200),
     APPLE_FARM("apple farm",0,Asset.WOOD,5,1, Texture.OVERGROWN_GRASSLAND,
             10,0,0,0,0, 5,0,0,
             0,0,null,Asset.APPLE,4,Category.FARM,
-            null,200),
+            200),
     DAIRY_PRODUCTS("dairy products",0,Asset.WOOD,10,1, Texture.OVERGROWN_GRASSLAND,
             10,0,0,0,0, 5,0,0,
             0,0,null, Asset.CHEESE,4,Category.DAIRY_PRODUCTS,
-            null,200),
+            200),
     HOP_FARM("hop farm",0,Asset.WOOD,15,1, Texture.OVERGROWN_GRASSLAND,10,
             0,0,0,0, 5,0,0,0,
-            0,null,Asset.HOPS,4,Category.FARM, null,200),
+            0,null,Asset.HOPS,4,Category.FARM, 200),
     HUNT_POST("hunt post",0,Asset.WOOD,10,1, Texture.GROUND,30,0,
             0,0,0, 5,0,0,0,0,
-            null,Asset.MEAT,1,Category.FARM, null,200),
+            null,Asset.MEAT,1,Category.FARM, 200),
     WHEAT_FARM("wheat farm",0,Asset.WOOD,10,1, Texture.OVERGROWN_GRASSLAND,
             10,0,0,0, 0,5,0,0,
-            0,0,null,Asset.WHEAT,4,Category.FARM, null,
+            0,0,null,Asset.WHEAT,4,Category.FARM,
             200),
     BAKERY("bakery",0, Asset.WOOD,10,1, Texture.GROUND,50,0,
             0,0,0, 5,1,0,0,0,
-            Asset.FLOUR,Asset.BREAD,2, Category.PROCESSING, null,200),
+            Asset.FLOUR,Asset.BREAD,2, Category.PROCESSING, 200),
     BREWERY("brewery",0,Asset.WOOD,10,1, Texture.GROUND,50,0,
             0,0,0, 1,1,0,0,
-            0,Asset.HOPS,Asset.ALE,2,Category.PROCESSING, null,200),
+            0,Asset.HOPS,Asset.ALE,2,Category.PROCESSING, 200),
     FOOD_STORAGE("food storage",0,Asset.WOOD,5,0, Texture.GROUND,50,
             200,0,0, 0, 0,0,0,0,
-            0,null,null,2, Category.STORAGE, null
+            0,null,null,2, Category.STORAGE
             ,100),
     KEEP("keep",0,Asset.STONE,0, 0,Texture.GROUND,100,0,0,
             0, 8,0,0,20,10,35,
-            null,null,2, Category.BUILDING, KeepMethods.getKeepMethods(),
+            null,null,2, Category.BUILDING,
             1000000),
     TREE("tree",0,null,0, 0,Texture.OVERGROWN_GRASSLAND,100,0,
             0, 0, 0,0,0,0,0
-            ,0, null,null,1, Category.BUILDING, null
+            ,0, null,null,1, Category.BUILDING
             ,1),
     WALL("wall",0,Asset.STONE,1, 0,Texture.GROUND,100,0,0,
             0, 0,0,0,20,10,4,
-            null,null,1, Category.WALL, null,30),
+            null,null,1, Category.WALL, 30),
     STAIR("stair",0,Asset.STONE,5, 0,Texture.GROUND,100,0,0,
             0, 0,0,0,20,10,10,
-            null,null,1, Category.STAIR, null,30)
+            null,null,1, Category.STAIR, 30)
     ;
     private final int workersNumber;
     private int hitpoint;
@@ -201,13 +201,12 @@ public enum BuildingType {
     private final Asset outputProduct;
     private final int width;
     private final Category category;
-    private final SelectBuildingMenuMethods selectBuildingMenuMethods;
 
     BuildingType(String name, int goldCost,Asset resourceType, int resourceCost,int workersNumber,
                  Texture groundType, int height, int capacity, int attackPoint, int popularityEffect,
                  int populationEffect, int productionRate, int producePrice, int fireRange, int defendRange,
-                 int soldiersCapacity, Asset productionAsset, Asset outputProduct, int width, Category category,
-                 SelectBuildingMenuMethods selectBuildingMenuMethods, int hitpoint) {
+                 int soldiersCapacity, Asset productionAsset, Asset outputProduct, int width, Category category
+                 , int hitpoint) {
         this.workersNumber = workersNumber;
         this.groundType = groundType;
         this.resourceType = resourceType;
@@ -228,7 +227,6 @@ public enum BuildingType {
         this.outputProduct = outputProduct;
         this.width = width;
         this.category = category;
-        this.selectBuildingMenuMethods = selectBuildingMenuMethods;
         this.hitpoint = hitpoint;
     }
 
@@ -313,10 +311,6 @@ public enum BuildingType {
 
     public Category getCategory() {
         return category;
-    }
-
-    public SelectBuildingMenuMethods getSelectBuildingMenuMethods() {
-        return selectBuildingMenuMethods;
     }
 
     public BuildingType getBuildingTypeByName(String name) {
