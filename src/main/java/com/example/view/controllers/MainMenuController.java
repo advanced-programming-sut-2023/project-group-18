c6da907abc9dde99dfc5d991400c37bfd1f5d753
Omidreza <omd.reza.masoumi@gmail.com>
2023-06-05 19:16:06 +0330

    start game

--- src/main/java/com/example/view/controllers/MainMenuController.java
+++ src/main/java/com/example/view/controllers/MainMenuController.java
@@ -1,10 +1,17 @@
 package com.example.view.controllers;
 
+import com.example.view.Main;
 import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
 
 public class MainMenuController {
 
-    public void profileMenu(MouseEvent mouseEvent) {
+    public void profileMenu(MouseEvent mouseEvent) throws IOException {
+        Main.goToMenu("ProfileMenu");
+    }
 
+    public void startGame(MouseEvent mouseEvent) throws IOException {
+        Main.goToMenu("StartGameMenu");
     }
 }
