c553879bb192f080a1d2911aa144dd490560d800
fraxea <moh788672@gmail.com>
2023-06-07 13:43:17 +0330

    add Centers

--- src/main/java/com/example/model/map/GameMap.java
+++ src/main/java/com/example/model/map/GameMap.java
@@ -1,5 +1,7 @@
 package com.example.model.map;
 
+import java.util.ArrayList;
+
 import com.example.model.Game;
 
 import javafx.beans.property.DoubleProperty;
@@ -12,15 +14,53 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
 public class GameMap extends Pane {
+    private static final double TILE_LENGTH = 50.0d;
     private final DoubleProperty scale;
+    private final ArrayList<Tile> centers;
     private final int length;
     private final Game game;
 
     public GameMap(int length, Game game) {
         this.scale = new SimpleDoubleProperty(1.0);
+        this.centers = new ArrayList<>();
         this.length = length;
         this.game = game;
         setInitScales();
+        initTiles();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private void initTiles() {
+        final double width = getBoundsInLocal().getWidth();
+        final double height = getBoundsInLocal().getHeight();
+        Canvas grid = new Canvas(width, height);
+        grid.setMouseTransparent(true);
+        GraphicsContext graphicsContext = grid.getGraphicsContext2D();
+        graphicsContext.setStroke(Color.GRAY);
+        graphicsContext.setLineWidth(1);
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                double x = (i % 2) * TILE_LENGTH / 2 + TILE_LENGTH * j;
+                double y = TILE_LENGTH * i / 2;
+                centers.add(new Tile(x, y, Texture.GROUND));
+                makeDiamond(graphicsContext, x, y);
+            }
+        }
+        getChildren().add(grid);
+        grid.toBack();
     }
 
     private void setInitScales() {
@@ -35,22 +75,10 @@ public class GameMap extends Pane {
         addEventFilters();
     }
 
-    private void addGrid() {
-        final double width = getBoundsInLocal().getWidth();
-        final double height = getBoundsInLocal().getHeight();
-        Canvas grid = new Canvas(width, height);
-        grid.setMouseTransparent(true);
-        GraphicsContext graphicsContext = grid.getGraphicsContext2D();
-        // graphicsContext.drawImage(null, h, h, w, h);
-        graphicsContext.setStroke(Color.GRAY);
-        graphicsContext.setLineWidth(1);
-        final double offset = 50;
-        for(double index = offset; index < width; index += offset) {
-            graphicsContext.strokeLine(index, 0, index, height);
-            graphicsContext.strokeLine(0, index, width, index);
-        }
-        getChildren().add(grid);
-        grid.toBack();
+    private void makeDiamond(GraphicsContext graphicsContext, double x, double y) {
+        double[] xPoints = {x - TILE_LENGTH, x, x + TILE_LENGTH, x};
+        double[] yPoints = {y, y + TILE_LENGTH / 2, y, y - TILE_LENGTH / 2};
+        graphicsContext.fillPolygon(xPoints, yPoints, 4);
     }
 
     private void addEventFilters() {
